2094ce371178c085a43cdf95e9d308cff9c43de3
Manda Kausthubh <manda.kausthubh@gmail.com>
2025-11-12 22:23:36 +0530

    Created main for splitting the function

---
+++ download_imagenetv2.py
@@ -0,0 +1,5 @@
+from .datasets.ImageNetV2 import save_split_indices
+
+if __name__ == "__main__":
+    root_dir = "./data/imagenetv2"
+    save_split_indices(root=root_dir, train_ratio=0.9, split_file="imagenetv2_splits.json")
